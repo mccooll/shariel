308d4bba434a7f48541387c2263d072aeadbcba0
mccooll <github@no3.ca>
2019-07-18 15:56:38 -0400

    simplest of encode/decode

--- src/Controller/OrgController.php
+++ src/Controller/OrgController.php
@@ -2,21 +2,31 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 use App\Model\Org;
+use App\Model\User;
 
 class OrgController
 {
-    public function encode()
+    private function setupOrg() {
+        $org = new Org('EP', null, []);
+        $user = new User('Dave', $org);
+        return $org;
+    }
+
+    public function encode(SerializerInterface $serializer)
     {
         return new Response(
-            'Encoded:'
+            'Encoded: '.htmlspecialchars($serializer->serialize($this->setupOrg(),'json'))
         );
     }
 
-    public function decode()
+    public function decode(SerializerInterface $serializer)
     {
+        $org = $serializer->serialize($this->setupOrg(),'json');
+        $org = $serializer->deserialize($org, Org::class, 'json');
         return new Response(
-            'Decoded:'
+            'Decoded: <pre>'. print_r($org, true)
         );
     }
 }
